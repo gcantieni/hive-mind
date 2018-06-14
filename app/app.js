f6f06e7b2c36f6c3252c7fadcf11ac95ca8da63b
gcantieni <gus.cantieni@gmail.com>
2018-06-13 20:00:30 -0400

    Clean draw function

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 const canvas = document.getElementById('board');
 const context = board.getContext('2d');
-const HIVE_WIDTH = 20;
-const VELOCITY = 10
 
-let player = {
+const HIVE_WIDTH = 20;
+const FLOWER_WIDTH = 5;
+const BEE_WIDTH = 5;
+
+const VELOCITY = 1;
+
+let hive = {
   pos: {x: canvas.width / 2, y: canvas.height / 2}
 }
 
@@ -14,48 +18,57 @@ let flower1 = {
 var flowers = [flower1];
 
 function draw() {
+  drawCanvas();
   drawFlowers(flowers);
-  drawPlayer(player);
+  drawHive(hive);
+}
+
+function drawBees() {
+
 }
 
 function drawFlowers(flowers) {
   let i = 0;
   for (i = 0; i < flowers.length; i++) {
     context.fillStyle = 'red';
-    context.fillRect(flowers[i].pos.x, flowers[i].pos.x,HIVE_WIDTH, HIVE_WIDTH);
+    context.fillRect(flowers[i].pos.x, flowers[i].pos.x, FLOWER_WIDTH, FLOWER_WIDTH);
   }
 }
 
-function drawPlayer(player) {
-  context.fillStyle = 'yellow';
-  context.fillRect(player.pos.x, player.pos.y, HIVE_WIDTH, HIVE_WIDTH);
+function drawHive(hive) {
+  context.fillStyle = '#DAA520';
+  context.fillRect(hive.pos.x, hive.pos.y, HIVE_WIDTH, HIVE_WIDTH);
 }
+
+function drawCanvas() {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 
 let lastTime = 0;
 function update(time=0) {
     const deltaTime = time - lastTime;
     lastTime = time;
 
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
     draw();
 
     requestAnimationFrame(update);
 }
 
+
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37) {
-    player.pos.x -= VELOCITY;
+    hive.pos.x -= VELOCITY;
   }
   else if (event.keyCode === 38) {
-    player.pos.y -= VELOCITY;
+    hive.pos.y -= VELOCITY;
   }
   else if (event.keyCode === 39) {
-    player.pos.x += VELOCITY;
+    hive.pos.x += VELOCITY;
   }
   if (event.keyCode === 40) {
-    player.pos.y += VELOCITY;
+    hive.pos.y += VELOCITY;
   }
 });
 
