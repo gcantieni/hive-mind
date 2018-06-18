ae042d9b07fc0f7d28663ed17ad23c80463831ac
gcantieni <gus.cantieni@gmail.com>
2018-06-17 22:50:59 -0400

    Implement modular design pattern

--- src/app/hive.js
+++ src/app/hive.js
@@ -1,10 +1,44 @@
 import * as constants from './constants.js';
 
-export function drawHives(context, hive) {
+export var hiveOrganizer = (function() {
+  let hives = [];
+
+  function addHive(x, y) {
+    hives.push({
+      pos: {x: x, y: y}
+    });
+  }
+
+  function removeHive(oldHive) {
+    var index = hives.indexOf(oldHive);
+    if (index > -1) {
+      oldHive.splice(index, 1);
+    }
+  }
+
+  function drawHives(context) {
+    context.fillStyle = '#DAA520';
+    hives.map((h) => {
+      context.fillRect(h.pos.x, h.pos.y,
+        constants.HIVE_WIDTH, constants.HIVE_WIDTH);
+    });
+  }
+
+  return {
+    add: addHive,
+    remove: removeHive,
+    draw: drawHives
+  }
+})()
+
+export function drawHives(context, hives) {
+  console.log(hives);
   context.fillStyle = '#DAA520';
-  context.fillRect(
-    hive.pos.x,
-    hive.pos.y,
-    constants.HIVE_WIDTH,
-    constants.HIVE_WIDTH);
+  for (let i = 0; i < hives.length; i += 1) {
+    context.fillRect(
+      hives[i].pos.x,
+      hives[i].pos.y,
+      constants.HIVE_WIDTH,
+      constants.HIVE_WIDTH);
+  }
 }
