733ecaa901389f64024cb18b8446ddbe0f2188db
gcantieni <gus.cantieni@gmail.com>
2018-06-20 19:59:52 -0400

    Eliminate over-object-oriented style

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,31 @@
-import { Game } from './game.js';
+import * as constants from './constants.js';
+import { HiveOrganizer } from './hive.js';
+import { FlowerOrganizer } from './flower.js';
+import { BeeOrganizer } from './bee.js';
+import { draw } from './draw.js';
 
-function launch() {
+
+function main() {
   const canvas = document.getElementById('board');
   const context = board.getContext('2d');
-  let game = new Game(canvas, context);
 
-  game.show();
+  const two = 2;
+  let hives = new HiveOrganizer(context);
+  let flowers = new FlowerOrganizer(context);
+  let bees = new BeeOrganizer(context, canvas);
+  let start = null;
+  update(0);
+  bees.listen();
+
+  function update(time = 0) {
+    let progress = time - start;
+    if (!start) start = time;
+    draw(context, canvas, hives, flowers, bees);
+    window.requestAnimationFrame(update)
+  }
 }
 
-launch();
+main();
+
+
+
