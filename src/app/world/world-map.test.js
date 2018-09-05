6ef3fc8073258a8a65bd0a9123c54f9e9c9fd7e5
gcantieni <gus.cantieni@gmail.com>
2018-09-05 09:39:14 -0400

    Remove slicing world

--- src/app/world/world-map.test.js
+++ src/app/world/world-map.test.js
@@ -3,9 +3,6 @@ import {
     idxToCoord,
     screenToWorld, 
     worldToScreen, 
-    sliceColsLeft, 
-    sliceWorldCols,
-    sliceWorldToSize,
     shouldDraw
   } from './world-map.js';
 import { forEach2D } from '../array2D.js';
@@ -51,29 +48,6 @@ test("screenToWorld should take screen x, y and return x, y within canvas",  () 
   expect(screenToWorld({x: 3, y: 2}, {x: 1, y: 1})).toEqual({x: 4, y: 3});
 });
 
-test("sliceColsLeft should return array with first n cols removed", () => {
-  expect(sliceColsLeft(1, 2, [0, 1, 2, 3])).toEqual([1, 3]);
-  var shouldBeUnchanged = [
-      0, 1, 2, 3,
-      4, 5, 6, 7
-    ];
-  expect(sliceColsLeft(2, 4, shouldBeUnchanged)).toEqual([2, 3, 6, 7]);
-  expect(shouldBeUnchanged).toEqual([0, 1, 2, 3, 4, 5, 6, 7]);
-});
-
-// test("sliceRowsTop should return array with first n rows removed", () => {
-//   expect.assertions(1);
-// });
-
-// test("sliceWorldRows should delete the first n rows of theh world and adjust rows var", () => {
-//   expect.assertions(1);
-// });
-
-test("sliceWorldCols should delete first n cols of world and change its cols var", () => {
-  expect(sliceWorldCols(1, { map: [0, 1, 2, 3, 4], rows: 2, cols: 2}))
-    .toEqual({ map: [1, 3], rows: 2, cols: 1});
-});
-
 test("shouldDraw should take index and return whether tile should be drawn", () => {
   var cam = { x: 1, y: 1, width: 2, height: 2 };
   expect(shouldDraw(cam, { x: 1, y: 1 })).toBe(true);
@@ -81,12 +55,4 @@ test("shouldDraw should take index and return whether tile should be drawn", () 
   expect(shouldDraw(cam, { x: 2, y: 2 })).toBe(true);
   expect(shouldDraw(cam, { x: 3, y: 1 })).toBe(false);
   expect(shouldDraw(cam, { x: 1, y: 3 })).toBe(false);
-});
-
-test("sliceWorldToSize should reduce the world to specified size starting from specific pt", () => {
-  expect(sliceWorldToSize( 
-    { x: 1, y: 1 }, 
-    { x: 2, y: 2 }, 
-    { map: [ 0, 1, 2, 3, 4, 5 ], rows: 2, cols: 3 }))
-    .toEqual( { map: [ 1, 2, 4, 5 ], rows: 2, cols: 2 } );
 });
