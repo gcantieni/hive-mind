3cc0b9e7945971706c06b8910b2c6033b36e2653
gcantieni <gus.cantieni@gmail.com>
2018-11-11 12:50:26 -0500

    Rewrite in functional style

--- src/app/clickable.js
+++ src/app/clickable.js
@@ -1,51 +1,20 @@
-import {Vec} from './math.js';
-export class Clickable {
-  constructor(x, y, width, height, color, clickablesMap) {
-    this.clickables = clickablesMap;
-    this.clickColor = null;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.canBeTarget = false;
-    this.center = this.center = new Vec(x, y);
-    this.addClickable();
-  }
-  draw(context, clickContext) {
-    context.fillStyle = this.color;
-    context.fillRect(
-      this.x,
-      this.y,
-      this.width,
-      this.height);
-    this.drawClickable(clickContext);
-  }
-  drawClickable(clickContext) {
-    clickContext.fillStyle = this.clickColor;
-    clickContext.fillRect(this.x, this.y, this.width, this.height);
-  }
-  addClickable() {
-    while(true) {
-      const colorKey = this.getRandomColor();
-      const hasKey = this.clickables.get(colorKey);
+const { Map } = require('immutable')
 
-      if (hasKey) {
-        continue;
-      }
-
-      this.clickables.set(colorKey, this);
-      this.clickColor = colorKey;
-      return;
-    }
-  }
-  handleClick(type) {
-    console.log(`clicked on a clickable game piece`);
-  }
-  getRandomColor() {
-   const r = Math.round(Math.random() * 255);
-   const g = Math.round(Math.random() * 255);
-   const b = Math.round(Math.random() * 255);
-   return `rgb(${r},${g},${b})`;
-  }
+function addClickable(clickMap, data) {
+	while(true) {
+		const colorKey = getRandomColor();
+		const hasKey = clickables.get(colorKey);
+		if (hasKey) {
+			continue;
+		}
+		return clickables.set(colorKey, data);
+	}
 }
+function getRandomColor() {
+	const r = Math.round(Math.random() * 255);
+	const g = Math.round(Math.random() * 255);
+	const b = Math.round(Math.random() * 255);
+	return `rgb(${r},${g},${b})`;
+}
+
+module.exports = addClickable
