b02de0e2a41b3b296f437cbefa4b6edc3adcef86
gcantieni <gus.cantieni@gmail.com>
2018-11-23 15:43:49 -0500

    Split worldmap off into module

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,5 @@
 //const { List, Map, Record } = require('immutable'); 
-const R = require('ramda'); 
+const world = require('./world/world-map.js');
 
 (async function main() {
 	const BOARD_WIDTH = 100;
@@ -22,80 +22,18 @@ const R = require('ramda');
 		]
 	}
 	var camera = {
-		x: 25,
-		y: 25,
+		x: 0,
+		y: 0,
 		width: 100,
 		height: 100,
 		maxX: 50,
 		maxY: 50
 	}
+
 	var tileAtlas = new Map()
 	tileAtlas.set(0, 'GreenYellow')
 	tileAtlas.set(1, 'Yellow')
 	tileAtlas.set(2, 'Orchid')
-
-
-	var getTileIndex = (row, col, cols) => row*cols + col
-	// val is not used but needed for 
-	var getRowCol = (cols, val, idx) => {
-		return { 
-			row: Math.floor(idx / cols), 
-			col: idx % cols, val: val
-		} 
-	}
-	getRowCol = R.curry(getRowCol)(worldMap.cols)
-
-	var visibleRange = (camStart, camSize, tsize) => {
-		return { 
-			start: Math.floor(camStart / tsize),
-			end: Math.ceil(camStart / tsize) + camSize / tsize
-		}
-	}
-
-	var colRange = visibleRange(camera.x, camera.width, TILE_SIZE)
-	var rowRange = visibleRange(camera.y, camera.height, TILE_SIZE)
-	var filterVisible = R.compose(
-		R.filter(
-			R.where({
-				row: R.both(
-					R.gt(R.__, rowRange.start - 1), 
-					R.lt(R.__, rowRange.end)),
-				col: R.both(
-					R.gt(R.__, colRange.start - 1), 
-					R.lt(R.__, colRange.end))
-			})),
-		R.addIndex(R.map)(getRowCol))
-
-	console.log(filterVisible(worldMap.layers))
-
-	var offset = R.curry(
-		(tsize, rangeStart, camPos) => 
-			-camPos + rangeStart * tsize)(TILE_SIZE)
-
-	var offsets = {
-		x: offset(colRange.start, camera.x),
-		y: offset(rowRange.start, camera.y)
-	}
-
-	var rowColValToXYVal = R.curry((tsize, cam, startRow, startCol, rowColVal) => {
-		return {
-			x: (rowColVal.col - startCol) * tsize 
-				+ offset(startCol, cam.x), 
-			y: (rowColVal.row - startRow) * tsize 
-				+ offset(startRow, cam.y), 
-			val: rowColVal.val
-		}
-	})(TILE_SIZE, camera, rowRange.start, colRange.start) 
-
-
-	var renderImage = R.curry((tsize, ctx, tatlas, XYVal) => {
-		ctx.fillStyle = tatlas.get(XYVal.val)
-		ctx.fillRect(XYVal.x, XYVal.y, tsize, tsize) 
-	})(TILE_SIZE, context, tileAtlas)
-	var renderMap = R.compose(
-		R.map(renderImage), 
-		R.map(rowColValToXYVal), 
-		filterVisible)
-
-	renderMap(worldMap.layers)
+	
+	world(worldMap, camera, tileAtlas, context, TILE_SIZE)
 })()
--- src/app/world/world-map.js
+++ src/app/world/world-map.js
@@ -1,28 +1,73 @@
-import { maybe } from "../util/functional.js";
-import { TILE_HEIGHT, TILE_WIDTH } from "./world-view.js";
+const R = require('ramda'); 
 
-export function validTile(type) {
-  return type == 'bee';
+function renderWorld(worldMap, camera, tileAtlas, context, tsize) {
+	var colRange = visibleRange(camera.x, camera.width, tsize);
+	var rowRange = visibleRange(camera.y, camera.height, tsize);
+	var getMapRowCol = getRowCol(worldMap.cols) // now just needs idx
+	var offsets = {
+		x: offset(colRange.start, camera.x),
+		y: offset(rowRange.start, camera.y)
+	};
+	var filterVisibleMap = filterVisible(rowRange, colRange, getMapRowCol)
+	var worldRCtoXY = rowColValToXYVal(tsize, camera, rowRange.start, colRange.start);
+
+	var renderWorldImage = renderImage(tsize, context, tileAtlas); // curried: now takesXYVal
+	var a = filterVisibleMap(worldMap.layers)
+	return R.compose(
+		R.map(renderWorldImage), 
+		R.map(worldRCtoXY), 
+		filterVisibleMap)(worldMap.layers);
 }
 
-export function idxToCoord(width, idx) {
-  return {
-    x: maybe(idx % width),
-    y: Math.floor(idx / width) 
-  };
+var getTileIndex = (row, col, cols) => row*cols + col
+// val is not used but needed for 
+var getRowCol = R.curry((cols, val, idx) => {
+	return { 
+		row: Math.floor(idx / cols), 
+		col: idx % cols, val: val
+	} 
+})
+
+var visibleRange = (camStart, camSize, tsize) => {
+	return { 
+		start: Math.floor(camStart / tsize),
+		end: Math.ceil(camStart / tsize) + camSize / tsize
+	}
 }
 
-export function worldToScreen(pos, camera) {
-  return {x: pos.x - camera.x, y: pos.y - camera.y};
-}
+var filterVisible = R.curry((rowRange, colRange, idxToRowCol, layer) => 
+	R.compose(
+		R.filter(
+			R.where({
+				row: R.both(
+					R.gt(R.__, rowRange.start - 1), 
+					R.lt(R.__, rowRange.end)),
+				col: R.both(
+					R.gt(R.__, colRange.start - 1), 
+					R.lt(R.__, colRange.end))
+			})),
+		R.addIndex(R.map)(idxToRowCol))(layer))
 
-export function screenToWorld(pos, camera) {
-  return {x: pos.x + camera.x, y: pos.y + camera.y};
-}
+var offset = R.curry(
+	(tsize, rangeStart, camPos) => 
+		-camPos + rangeStart * tsize)
 
-export function shouldDraw(camera, pos) {
-  return pos.x >= Math.floor(camera.x) && 
-    pos.x <= Math.ceil(camera.x + camera.width - 1) &&
-    pos.y >= Math.floor(camera.y) &&
-    pos.y <= Math.ceil(camera.y + camera.height - 1);
-};
+
+var rowColValToXYVal = R.curry((tsize, cam, startRow, startCol, rowColVal) => {
+	var getOffset = offset(tsize)
+	return {
+		x: (rowColVal.col - startCol) * tsize 
+			+ getOffset(startCol, cam.x), 
+		y: (rowColVal.row - startRow) * tsize 
+			+ getOffset(startRow, cam.y), 
+		val: rowColVal.val
+	}
+})
+
+var renderImage = R.curry((tsize, ctx, tatlas, XYVal) => {
+	ctx.fillStyle = tatlas.get(XYVal.val)
+	ctx.fillRect(XYVal.x, XYVal.y, tsize, tsize) 
+})
+
+module.exports = renderWorld
+
